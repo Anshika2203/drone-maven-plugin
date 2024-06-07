eccea9b1940d6b71bf1791ad21a383bed2b3ec67
Anshika Anand <anshika.anand@harness.io>
2024-06-07 19:18:27 +0530

    Creating plugin

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,11 @@
-# Use golang:1.22 as the builder stage
-FROM golang:1.22 as builder
+# Use the Maven base image for the final stage
+FROM maven:3.9.7
 
-# Set the working directory
-WORKDIR /app
-
-# Copy go.mod and go.sum files
-COPY go.mod go.sum ./
-
-# Download all dependencies (go.sum may be empty if there are no dependencies)
-RUN go mod download
-
-# Copy the source code
-COPY . .
-
-# Build the Go executable for amd64 architecture
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o /app/drone-maven-plugin ./main.go
-
-# Use openjdk:latest as the base image
-FROM maven:3.8.4-jdk-11
+# Set the working directory inside the container
+#WORKDIR /app
 
 # Copy the built Go executable from the builder stage to the final stage
-COPY --from=builder /app/drone-maven-plugin /app/
+COPY drone-maven-plugin /bin/
 
 # Set the entrypoint to the Go executable
-ENTRYPOINT ["/app/drone-maven-plugin"]
+ENTRYPOINT ["/bin/drone-maven-plugin"]
--- main.go
+++ main.go
@@ -2,57 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
-	"strings"
 )
 
 func main() {
-	// Function to initialize Maven settings
-	initMavenSettings()
-
-	// Construct the Maven command
-	mvnCommand := "mvn -B"
-
-	// Set default goals if not provided
-	if os.Getenv("PLUGIN_GOALS") == "" {
-		fmt.Println("No $PLUGIN_GOALS defined, using default goals -DskipTests clean install")
-		os.Setenv("PLUGIN_GOALS", "-DskipTests clean install")
-	} else {
-		os.Setenv("PLUGIN_GOALS", strings.ReplaceAll(os.Getenv("PLUGIN_GOALS"), ",", " "))
+	buildCmd := exec.Command("mvn", "clean install")
+	buildOutput, buildErr := buildCmd.CombinedOutput()
+	if buildErr != nil {
+		fmt.Println("Error running 'mvn clean install':", buildErr)
+		fmt.Println(string(buildOutput))
+		return
 	}
-	mvnCommand += " " + os.Getenv("PLUGIN_GOALS")
+	fmt.Println("Output of 'mvn clean install':", string(buildOutput))
 
-	// Add Maven modules if provided
-	if os.Getenv("PLUGIN_MAVEN_MODULES") != "" {
-		mvnCommand += " -pl " + os.Getenv("PLUGIN_MAVEN_MODULES")
+	versionCmd := exec.Command("mvn", "--version")
+	versionOutput, versionErr := versionCmd.CombinedOutput()
+	if versionErr != nil {
+		fmt.Println("Error running 'maven -version':", versionErr)
+		fmt.Println(string(versionOutput))
+		return
 	}
-
-	// Add context directory if provided
-	if os.Getenv("PLUGIN_CONTEXT_DIR") != "" {
-		mvnCommand += " -f " + os.Getenv("PLUGIN_CONTEXT_DIR")
-	}
-
-	// Print the Maven command if debug mode is enabled
-	if os.Getenv("PLUGIN_LOG_LEVEL") == "debug" {
-		fmt.Println("Running command:", mvnCommand)
-	}
-
-	// Execute the Maven command
-	cmd := exec.Command("bash", "-c", mvnCommand)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-}
-
-// Function to initialize Maven settings
-func initMavenSettings() {
-	// Ensure MAVEN_CONFIG is set to a default value if not provided
-	if os.Getenv("MAVEN_CONFIG") == "" {
-		os.Setenv("MAVEN_CONFIG", "/root/.m2")
-	}
+	fmt.Println("Output of 'maven -version':", string(versionOutput))
 }
